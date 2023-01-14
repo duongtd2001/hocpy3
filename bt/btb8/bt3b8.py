f04ce3fabb742cf07c3f0bb8068575da97af1309
duongtd2001 <duongadtdcf2001@gmail.com>
2023-01-14 20:34:31 +0700

    init commit

--- bt/btb8/bt3b8.py
+++ bt/btb8/bt3b8.py
@@ -1,4 +1,11 @@
 # Nhập hai số a và b từ bàn phím. In ra số lớn nhất và nhỏ nhất trong hai số
 a = int(input('nhập a: '))
 b = int(input('nhập b: '))
-print()
+if a > b:
+    print('số lớn nhất là:', a)
+else:
+    print('số nhỏ nhất là:', a)
+if a < b:
+    print('số lớn nhất là:', b)
+else:
+    print('số nhỏ nhất là:', b)
